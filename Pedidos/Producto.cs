b5c91bacad91a82c79aea983ae5c7dfe9e631031
Jason Madrid <jasonhnn6@gmail.com>
2025-08-04 21:26:37 -0600

    Adding interface

--- Pedidos/Producto.cs
+++ Pedidos/Producto.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Pedidos
 {
-    public abstract class Producto
+    public abstract class Producto : IInformacionDetallada
     {
         public int Id { get; set; }
         public string Nombre { get; set; }
@@ -23,6 +23,14 @@ namespace Pedidos
                 Console.WriteLine($"No hay suficiente stock de {Nombre}");
                 Console.WriteLine();
             }
+        }
+
+        public string ObtenerInformacionDetallada()
+        {
+            return $"ID: {Id}\n" +
+                   $"Nombre: {Nombre}\n" +
+                   $"Precio: {Precio:C}\n" +
+                   $"Cantidad disponible: {Stock}";
         }
     }
 }
